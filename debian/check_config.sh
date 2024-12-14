a6023cb48441b7de8f67f0f748f54fc93d2863d8
Mendex777 <30417083+Mendex777@users.noreply.github.com>
2024-12-14 18:31:33 +0300

    Update check_config.sh

--- debian/check_config.sh
+++ debian/check_config.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# 定义颜色
+# Определить цвета
 CYAN='\033[0;36m'
 RED='\033[0;31m'
-NC='\033[0m' # No Color
+NC='\033[0m' # Без цвета
 
 CONFIG_FILE="/etc/sing-box/config.json"
 
-# 检查配置文件是否存在
+# Проверить, существует ли конфигурационный файл
 if [ -f "$CONFIG_FILE" ]; then
-    echo -e "${CYAN}检查配置文件 ${CONFIG_FILE} ...${NC}"
-    # 验证配置文件
+    echo -e "${CYAN}Проверка конфигурационного файла ${CONFIG_FILE} ...${NC}"
+    # Проверить конфигурационный файл
     if sing-box check -c "$CONFIG_FILE"; then
-        echo -e "${CYAN}配置文件验证通过！${NC}"
+        echo -e "${CYAN}Проверка конфигурационного файла пройдена!${NC}"
     else
-        echo -e "${RED}配置文件验证失败！${NC}"
+        echo -e "${RED}Проверка конфигурационного файла не удалась!${NC}"
         exit 1
     fi
 else
-    echo -e "${RED}配置文件 ${CONFIG_FILE} 不存在！${NC}"
+    echo -e "${RED}Конфигурационный файл ${CONFIG_FILE} не существует!${NC}"
     exit 1
 fi

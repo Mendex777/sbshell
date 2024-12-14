0c4ba2d8b1f893f316182f4a611a623056426913
Mendex777 <30417083+Mendex777@users.noreply.github.com>
2024-12-14 18:45:04 +0300

    Update set_defaults.sh

--- debian/set_defaults.sh
+++ debian/set_defaults.sh
@@ -2,20 +2,20 @@
 
 DEFAULTS_FILE="/etc/sing-box/defaults.conf"
 
-# 提示用户输入参数，如果为空则使用默认值
-read -rp "请输入后端地址: " BACKEND_URL
+# Попросить пользователя ввести параметры, если они пусты, использовать значения по умолчанию
+read -rp "Пожалуйста, введите адрес бэкенда: " BACKEND_URL
 BACKEND_URL=${BACKEND_URL:-$(grep BACKEND_URL $DEFAULTS_FILE | cut -d '=' -f2)}
 
-read -rp "请输入订阅地址: " SUBSCRIPTION_URL
+read -rp "Пожалуйста, введите адрес подписки: " SUBSCRIPTION_URL
 SUBSCRIPTION_URL=${SUBSCRIPTION_URL:-$(grep SUBSCRIPTION_URL $DEFAULTS_FILE | cut -d '=' -f2)}
 
-read -rp "请输入TProxy配置文件地址: " TPROXY_TEMPLATE_URL
+read -rp "Пожалуйста, введите адрес конфигурационного файла TProxy: " TPROXY_TEMPLATE_URL
 TPROXY_TEMPLATE_URL=${TPROXY_TEMPLATE_URL:-$(grep TPROXY_TEMPLATE_URL $DEFAULTS_FILE | cut -d '=' -f2)}
 
-read -rp "请输入TUN配置文件地址: " TUN_TEMPLATE_URL
+read -rp "Пожалуйста, введите адрес конфигурационного файла TUN: " TUN_TEMPLATE_URL
 TUN_TEMPLATE_URL=${TUN_TEMPLATE_URL:-$(grep TUN_TEMPLATE_URL $DEFAULTS_FILE | cut -d '=' -f2)}
 
-# 更新默认配置文件
+# Обновить файл конфигурации по умолчанию
 cat > $DEFAULTS_FILE <<EOF
 BACKEND_URL=$BACKEND_URL
 SUBSCRIPTION_URL=$SUBSCRIPTION_URL
@@ -23,4 +23,4 @@ TPROXY_TEMPLATE_URL=$TPROXY_TEMPLATE_URL
 TUN_TEMPLATE_URL=$TUN_TEMPLATE_URL
 EOF
 
-echo "默认配置已更新"
+echo "Конфигурация по умолчанию обновлена"
